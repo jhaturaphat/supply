46364e115c7c10bc52d18639f5ae353cef47ba94
umt.eng52@gmail.com <umt.eng52@gmail.com>
2021-05-11 14:27:30 +0700

    end code

--- assets/js/include.js
+++ assets/js/include.js
@@ -2,6 +2,32 @@ class Navbar extends HTMLElement {
     connectedCallback(){
         this.innerHTML = `
                  
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top fixed-top">
+                <a class="navbar-brand" href="#">ระบบเบิกเซทและอุปกรณ์จ่ายกลาง</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/fontend/index.html"> <i class='bi bi-cart-check-fill'></i> ส่งใบเบิก <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/fontend/ward/order.html"> <i class='bi bi-clock-history'></i> ประวัติการเบิก <span class="sr-only">(current)</span></a>
+                        </li>                        
+                    </ul>
+                </div>
+            </nav>
+        
+        `;
+    }
+}
+
+class NavbarBackend extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `
+                 
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
                 <a class="navbar-brand" href="#">ระบบเบิกเซทและอุปกรณ์จ่ายกลาง</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,26 +38,7 @@ class Navbar extends HTMLElement {
         `;
     }
 }
-class Cssplugins extends HTMLElement{
-    connectedCallback(){
-        this.innerHTML = `
-        <link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap.min.css">    
-        <link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap-grid.min.css">
-        <link rel="stylesheet" href="../../assets/bootstrap/font/bootstrap-icons.css">
-        `;
-    }
-}
 
-// class Jsplugins extends HTMLElement {
-//     constructor(){
-//         this.innerHTML = `
-//         <script src="../../assets/js/jquery-3.6.0.min.js"></script>
-//         <script src="../../assets/bootstrap/js/bootstrap.min.js"></script>
-//         <script src="../../assets/js/sweetalert.min.js"></script>
-//         `;
-//     }
-// }
 
 customElements.define('nav-bar', Navbar);
-customElements.define('css-plugins', Cssplugins);
-//customElements.define('js-plugins', Jsplugins);
+customElements.define('nav-bar-backend', NavbarBackend);
